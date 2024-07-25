068772ca3b614e9c52a2c168eecd9ff990e31e8d
Gaurav918 <gauravsingh181997@gmail.com>
2024-07-25 15:25:19 +0530

    login changes and security added

--- src/home/Home.js
+++ src/home/Home.js
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
 import axiosGetAllUserDetail from "../axios/axiosGetLimitedUserDetail";
 import { PleaseWaitContext } from "../context/PleaseWaitContextProvider.js";
-import { LOGIN_URL,CHECK_AUTHENTICATION_URL } from '../constants/Constants';
+import { LOGIN_URL,CHECK_AUTHENTICATION_URL,PARENT_DOMAIN } from '../constants/Constants';
+import Cookies from 'js-cookie';
 
 export default function Home(props) {
 
@@ -14,6 +15,14 @@ export default function Home(props) {
     setTimeout(() => {
         setErrMsg("")
     }, 4000)
+
+    const handleClick = () => {
+        Cookies.set("loginButton", "yatra", {
+          expires: 1,
+          domain: PARENT_DOMAIN,
+          path: "/",
+        });
+      };
 
     const fetchData = async()=>{
         setGWaitOn(true);
@@ -33,7 +42,7 @@ export default function Home(props) {
     }
 
     useEffect(() =>{
-         fetchData();
+         fetchData().catch((e)=>console.log("you are not loggedIn"));
     }, []);
 
     return (
@@ -41,14 +50,14 @@ export default function Home(props) {
             <div className="row pt-5">
                 <div className="col-lg-4 offset-lg-4 col-md-6 offset-md-3 col-sm-6 offset-sm-3 col-xs-6 offset-xs-3">
                     <div className="card text-center mx-auto" >
+                    <h5 hidden={errMsg.length != 0 ? false : true} style={{ color: "red" }}>{errMsg}</h5>
                         <div className="card-body login-card-body">
                             <h3>Welcome</h3>
                             <p className="mt-4">Login to your Account!!</p>
-                            <button className="google-login-button" type='button' text='Login'><a style={{color:"white"}}href={LOGIN_URL}>Login with Google</a></button>
+                            <button className="google-login-button" type='button' text='Login' onClick={handleClick}><a style={{color:"white"}}href={LOGIN_URL}>Login with Google</a></button>
                         </div>
                     </div>
                 </div>
-                <h6 hidden={errMsg.length != 0 ? false : true} style={{ color: "red" }}>{errMsg}</h6>
             </div>
         </>
     )
